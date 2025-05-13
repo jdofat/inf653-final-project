7b4354e3f7ceeb95d2c9292195b90c380feb8187
jdofat <143310236+jdofat@users.noreply.github.com>
2025-05-13 11:50:08 -0400

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+console.log("xxx");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    console.log("sss");
+    await mongoose.connect(process.env.DATABASE_URI);
+    console.log("MongoDB connected");
   } catch (err) {
+    console.log("vvv");
     console.error(err);
   }
 };
-
+console.log("eee");
 module.exports = connectDB;
